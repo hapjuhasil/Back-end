55bc547a006948944bfc81594b885e8f24f33fde
sebbbin <nsb08045@daum.net>
2024-02-01 23:22:35 +0900

    init : 초기 설정

---
+++ server/src/main/java/com/hapjuhasil/server/HapjuhasilApplication.java
@@ -0,0 +1,13 @@
+package com.hapjuhasil.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HapjuhasilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HapjuhasilApplication.class, args);
+	}
+
+}
